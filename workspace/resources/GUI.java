11358fda48e87d45ee528f458ccc3db1b6d029f5
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2025-10-16 16:49:39 +0000

    yaya ya

--- workspace/resources/GUI.java
+++ workspace/resources/GUI.java
@@ -1,6 +1,7 @@
 package resources;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -15,32 +16,50 @@ import java.net.URL;
 
 public class GUI extends JFrame implements ActionListener, MouseListener, MouseMotionListener{
 
+	// What game the GUI is apart of 
 	Solitaire game;
-   public GUI(Solitaire game){
-	   this.game= game;
-        //Create and set up the window.
-       setTitle("Solitaire");
-       setSize(900,700);
-       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
-       // this supplies the background
-       try {
-		System.out.println(getClass().toString());
-		Image blackImg = ImageIO.read(getClass().getResource("background.jpg"));
-		setContentPane(new ImagePanel(blackImg));
 
-       }catch(IOException e) {
-    	   e.printStackTrace();
-       }
+	// Constructor for a GUI
+	public GUI(Solitaire game){
+		// a GUI IS A J FRAME! All this info is something you would change of a JFrame, but here we do it as a GUI which has set settings
+	   this.game= game;
+
+        //Create and set up the window.
+       	setTitle("Solitaire");
+       	setSize(900,700);
+       	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.getContentPane().setLayout(new BorderLayout());
+		getContentPane().setComponentOrientation(          java.awt.ComponentOrientation.RIGHT_TO_LEFT);
+
+       
+    //    this supplies the background
+	// CURRENTLY BACKGROUND CAUSES ERRORS!!!!!!!!!!!!!!!!!!
+    //    try {
+	// 	System.out.println(getClass().toString());
+	// 	ImageIcon backImage = new ImageIcon();
+	// 	Image blackImg = ImageIO.read(getClass().getResource("background.jpg"));
+	// 	setContentPane(new ImagePanel(blackImg));
+
+    //    }catch(IOException e) {
+    // 	   e.printStackTrace();
+    //    }
        
        /*******
         * This is just a test to make sure images are being read correctly on your machine. Please replace
         * once you have confirmed that the card shows up properly. The code below should allow you to play the solitare
         * game once it's fully created.
         */
+
+	   //Card is a custom JComponent
        Card card = new Card(2, Card.Suit.Diamonds);
-       System.out.println(card);
-       this.add(card);    
+      // System.out.println(card);
+	  JPanel newPanel = new JPanel();
+	  card.setPreferredSize(new Dimension(200, 200));
+	  newPanel.add(card);
+	  newPanel.setPreferredSize(new Dimension(200, 200));
+	  newPanel.setSize(200, 200);
+	  newPanel.setBorder(BorderFactory.createMatteBorder(2, 2, 2,2, Color.BLUE ));
+       this.add(newPanel, BorderLayout.EAST);    
 
         this.setVisible(true);
     }
